51734aadbe70b6da7195f2d96b18d3d403e0b047
Luke <goodmalu@oregonstate.edu>
2026-04-17 08:51:40 -0400

    Infra: Implement document-level keydown event delegation

--- scripts/document.js
+++ scripts/document.js
@@ -1,5 +1,26 @@
-console.log("Infrastructure loaded.");
-function init() {
-    console.log("System initialized...");
-}
-init();
+/*************************************************************************
+ * File: document.js
+ * These functions support interaction with the top-level document
+ * element.
+ ************************************************************************/
+
+/*************************************************************************
+ * @function Document Keydown Event Handler 
+ * @desc
+ * When the user presses a key in the app, we interpret the
+ * keypress based on which user interface element currently has focus. 
+ *************************************************************************/
+document.addEventListener("keydown", function(e) {
+    if (document.activeElement.id === "menuBtn") {
+        //User is pressing a key when menu button is focused
+        keyDownMenuBtnFocused(e.code);
+    } else if (document.activeElement.getAttribute("role") === "menuitem") {
+        //User is pressing a key when menu item is focused
+        keyDownMenuItemFocused(e.code);
+    } else if (document.activeElement.getAttribute("role") === "tab") {
+        //User is pressing a key when mode tab is focused
+        keyDownModeTabFocused(e.code);
+    } else if (document.activeElement.classList.contains("action-dialog")) {
+        keyDownDialogFocused(e);
+    }
+});
