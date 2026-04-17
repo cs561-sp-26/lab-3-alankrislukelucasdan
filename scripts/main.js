83e0fd61a2a03fad21331567322af7c32696f331
Luke <goodmalu@oregonstate.edu>
2026-04-17 08:52:34 -0400

    Infra: Define global app state and menu variables

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/*************************************************************************
+ * File: main.js
+ * Definitions of variables to maintain app state and provide
+ * convenient access to frequently used DOM elements.
+ *************************************************************************/
+let GlobalHistoryLogging = true;
+let GlobalDialogClose = null;
+
+/************************************/
+/* MENU VARIABLES */
+/************************************/
+const GlobalMenuBtn = document.getElementById("menuBtn");
+const GlobalMenuItems = document.querySelectorAll("li[role='menuitem']");
